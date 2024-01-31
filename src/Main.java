232354776f18dc91ec3624938698528c71b7e787
DarstB <bradendarst10@gmail.com>
2024-01-31 10:33:09 -0500

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int numToExamine = 100;
+        int evenChecker = numToExamine % 2;
+
+        System.out.println("If the output is 0, the number is even. Otherwise, it is odd. The output is: " + evenChecker);
+    }
+}
