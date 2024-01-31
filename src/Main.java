2fe57c0ef07c55fdd91e08305416d28a5f2685c3
DarstB <bradendarst10@gmail.com>
2024-01-31 10:37:04 -0500

    Update

--- src/Main.java
+++ src/Main.java
@@ -4,5 +4,6 @@ public class Main {
         int evenChecker = numToExamine % 2;
 
         System.out.println("If the output is 0, the number is even. Otherwise, it is odd. The output is: " + evenChecker);
+
     }
 }
